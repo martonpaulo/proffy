0841e4e0de3e449c280f14ca83666f063ffe0d24
Marton <martonpss@gmail.com>
2020-08-09 13:10:20 -0300

    Fifth day

--- src/components/PageHeader/index.tsx
+++ src/components/PageHeader/index.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 import { View, Image, Text } from 'react-native';
 import { BorderlessButton } from 'react-native-gesture-handler';
 import { useNavigation } from '@react-navigation/native';
@@ -11,10 +11,11 @@ import styles from './styles';
 
 interface PageHeaderProps {
 	title: string;
+	headerRight?: ReactNode;
 }
 
 
-const PageHeader: React.FC<PageHeaderProps> = ({ title }) => {
+const PageHeader: React.FC<PageHeaderProps> = ({ title, headerRight, children }) => {
 	
 	const { navigate } = useNavigation();
 	
@@ -32,7 +33,12 @@ const PageHeader: React.FC<PageHeaderProps> = ({ title }) => {
 				<Image source={logoImg} resizeMode="contain" />
 			</View>
 
-			<Text style={styles.title}>{title}</Text>
+			<View style={styles.header}>
+				<Text style={styles.title}>{title}</Text>
+				{headerRight}
+			</View>
+
+			{children}
 		</View>
 	);
 }
--- src/components/PageHeader/styles.ts
+++ src/components/PageHeader/styles.ts
@@ -4,7 +4,6 @@ const styles = StyleSheet.create({
 	container: {
 		padding: 40,
 		backgroundColor: '#8257E5',
-
 	},
 
 	topBar: {
@@ -13,13 +12,19 @@ const styles = StyleSheet.create({
 		justifyContent: 'space-between'
 	},
 
+	header: {
+		flexDirection: 'row',
+		alignItems: 'center',
+		justifyContent: 'space-between',
+	},
+
 	title: {
 		fontFamily: 'Archivo_700Bold',
 		color: '#FFF',
 		fontSize: 24,
 		lineHeight: 32,
 		maxWidth: 160,
-		marginVertical: 40,
+		marginVertical: 20,
 	},
 });
 
